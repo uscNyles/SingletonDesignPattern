f2f65a122392cf73ff40fe49a6768256b655e678
nylesgeiger <nylesgeiger@10.36.227.4>
2020-10-03 21:56:46 -0400

    Created all the files and finished the patient class

---
+++ SingletonDesignPatternOct3/src/HospitalSystem.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class HospitalSystem {
+
+	private ArrayList<Patient> patients;
+	private HospitalSystem hospitalsystem;
+	
+	public HospitalSystem() {
+		patients = new ArrayList<Patient>() ; 
+	}
+	
+	public HospitalSystem getInstance() {
+		
+	}
+	
+	public int addPatient(String firstName, String LastName) {
+		
+	}
+	public Patient getRecord(int id) {
+		
+	}
+	
+	public void addAllergy(int id, String allergy) {
+		
+	}
+	
+	public String getAllPatientsData() {
+		
+	}
+	
+	
+	
+}
---
+++ SingletonDesignPatternOct3/src/Patient.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Patient {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private ArrayList<String> allergies;
+	
+	public Patient(int id, String firstName, String lastName) {
+		
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		allergies = new ArrayList<String>();
+	}
+	
+	public void addAllergy(String allergy) {
+		this.allergies.add(allergy);
+	}
+	
+	public String toString() {
+		String toReturn = "";
+		Iterator<String> Josh = allergies.iterator();
+		
+		for( String allergy: allergies) {
+			if(Josh.hasNext()) {
+			toReturn = allergy + ',';
+			} else {
+			toReturn = allergy;
+			}
+			Josh.next();
+		}
+		
+		return "Patient " + this.id + ": " + this.firstName + " " + this.lastName + "\n" +"  Allergies: " + toReturn;
+	}
+}
