80b4fc7f766af075def87eda1f3d31cea87580ed
nylesgeiger <nylesgeiger@10.36.227.4>
2020-10-03 23:14:15 -0400

    All done except JavaDOC

--- SingletonDesignPatternOct3/src/Patient.java
+++ SingletonDesignPatternOct3/src/Patient.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class Patient {
 
@@ -7,32 +8,35 @@ public class Patient {
 	private String firstName;
 	private String lastName;
 	private ArrayList<String> allergies;
-	
+
 	public Patient(int id, String firstName, String lastName) {
-		
+
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		allergies = new ArrayList<String>();
 	}
-	
+
 	public void addAllergy(String allergy) {
 		this.allergies.add(allergy);
 	}
-	
+
 	public String toString() {
 		String toReturn = "";
-		Iterator<String> Josh = allergies.iterator();
-		
-		for( String allergy: this.allergies) {
-			if(Josh.hasNext()) {
-			toReturn += allergy + ',';
-			} else {
-			toReturn = allergy;
-			}
-			Josh.next();
+		if (this.allergies.size() > 0) {
+			toReturn = "Allergies: ";
 		}
-		
-		return "Patient " + (this.id+1) + ": " + this.firstName + " " + this.lastName + "\n" +"  Allergies: " + toReturn;
+
+		ListIterator<String> Josh = allergies.listIterator();
+		for (String allergy : this.allergies) {
+			Josh.next();
+			if (Josh.hasNext()) {
+				toReturn += allergy + ", ";
+			} else {
+				toReturn += allergy;
+			}
+		}
+
+		return "Patient " + (this.id + 2) + ": " + this.firstName + " " + this.lastName + "\n" + "  " + toReturn;
 	}
 }
